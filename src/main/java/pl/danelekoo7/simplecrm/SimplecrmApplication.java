9c11e2d2318c7c5c5d0bed06095565fa73a9751f
danelekoo7 <danieljedrus@gmail.com>
2020-04-30 18:24:17 +0200

    * Spring Boot project created * initialize DB

---
+++ src/main/java/pl/danelekoo7/simplecrm/SimplecrmApplication.java
@@ -0,0 +1,13 @@
+package pl.danelekoo7.simplecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplecrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/danelekoo7/simplecrm/SimplecrmApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.danelekoo7.simplecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
